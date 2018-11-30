9a58f8b7796c0d34bcd2bcb78caa1c44255b64e2
hxtruong <hoangxuantruong98@gmail.com>
2018-12-01 00:32:43 +0700

    find the nearest human - in processing

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -1,19 +1,29 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
-public class PlayerBehaviour : MonoBehaviour {
+public class PlayerBehaviour : MonoBehaviour
+{
     public float speed;
     public GameObject flashlight;
     public bool lightOn;
-    public List<GameObject> human;
+
+    [SerializeField] private GameObject humanPrefab;
+    [HideInInspector] public List<GameObject> human = new List<GameObject>();
     private Rigidbody2D rb2d;
     private bool isDead;
-    
 
-	// Use this for initialization
-	void Start () {
+
+    // Use this for initialization
+    void Start()
+    {
         rb2d = this.gameObject.GetComponent<Rigidbody2D>();
-	}
+        var humInstantiate = Instantiate(humanPrefab, Vector3.zero, Quaternion.identity);
+        humInstantiate.transform.parent = this.transform;
+        human.Add(humInstantiate);
+        for (int i = 0; i < 1; i++)
+        {
+            AddingHuman();
+        }
+    }
 
     // Update is called once per frame
     void Update()
@@ -28,7 +38,8 @@ public class PlayerBehaviour : MonoBehaviour {
         //rb2d.rotation = Quaternion.Lerp(rb2d.transform.rotation + tempVect);
         float angle = Mathf.Atan2(tempVect.y, tempVect.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-        if (Input.GetKeyDown(KeyCode.Space)) {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
             lightOn = !lightOn;
             FlashLight(lightOn);
         }
@@ -38,6 +49,19 @@ public class PlayerBehaviour : MonoBehaviour {
     {
         if (lightOn) flashlight.gameObject.SetActive(true);
         else flashlight.gameObject.SetActive(false);
+    }
+
+    public void AddingHuman()
+    {
+        float x = human[human.Count - 1].transform.position.x;
+        float y = human[human.Count - 1].transform.position.y;
+        float addingRadius = human[human.Count - 1].GetComponent<HumanBehaviour>().circleColliderRadius;
+        print("forward: " + human[human.Count - 1].transform.forward);
+        print("pos: " + human[human.Count - 1].transform.position);
+        Vector3 nextPosHuman = -human[human.Count - 1].transform.forward + new Vector3(addingRadius, addingRadius, 0);
+        var humanInstantiate = Instantiate(humanPrefab, nextPosHuman, Quaternion.identity);
+        humanInstantiate.transform.parent = this.transform;
+        human.Add(humanInstantiate);
     }
 
 }
