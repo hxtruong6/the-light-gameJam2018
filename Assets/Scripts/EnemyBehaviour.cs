0e37060ab9e07a0f1ea6ab95bba9e8d0254c271d
hxtruong <hoangxuantruong98@gmail.com>
2018-11-30 23:01:20 +0700

    adding enemy moving

---
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour
+{
+
+    [SerializeField] private GameObject player;
+
+    [SerializeField] private float speed;
+    // Use this for initialization
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").gameObject;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        var playerPos = player.transform;
+        // TODO: find the nearest human
+        transform.position = Vector2.MoveTowards(transform.position, playerPos.position, speed * Time.deltaTime);
+    }
+
+    public GameObject FindClosestPlayer()
+    {
+        GameObject[] gos;
+        gos = GameObject.FindGameObjectsWithTag("Human");
+        // TODO: get list gameobjects from Player
+        GameObject closest = null;
+        float distance = Mathf.Infinity;
+        Vector3 position = transform.position;
+        foreach (GameObject go in gos)
+        {
+            Vector3 diff = go.transform.position - position;
+            float curDistance = diff.sqrMagnitude;
+            if (curDistance < distance)
+            {
+                closest = go;
+                distance = curDistance;
+            }
+        }
+        return closest;
+    }
+
+}
---
+++ Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    [SerializeField] private GameObject enemy;
+
+    [SerializeField] private float radius;
+
+    private float timeCount = 5;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeCount > 2.0f)
+        {
+            RandomEnemy();
+            timeCount = 0;
+        }
+        timeCount += Time.deltaTime;
+    }
+
+    void RandomEnemy()
+    {
+        var pos2D = new Vector3(Random.Range(-radius, radius), Random.Range(-radius, radius), 0);
+        var rootPos = new Vector3(0, 0, 0);
+        //TODO: check player position
+        var enemyClone =  Instantiate(enemy, pos2D + rootPos, Quaternion.identity);
+        enemyClone.transform.parent = this.transform;
+
+    }
+}
